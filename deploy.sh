6a87fa36f08e229bf943860d774f1e9b6292ea2c
shixiaoqi <s18634879388@sina.com>
2016-09-01 12:46:52 +0800

    push3

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ ps -ef|grep sxqTest-1.0-SNAPSHOT.jar|awk '{print $2}'|xargs kill -9
 
 mvn package
 
-cp target/sxqTest-1.0-SNAPSHOT.jar /root/sxqtest/sxqTest/sxqTest-1.0-SNAPSHOT.jar
+cp target/sxq_test-1.0-SNAPSHOT.jar /root/sxqtest/sxqTest/sxqTest-1.0-SNAPSHOT.jar
 
 nohup java -jar /root/sxqtest/sxqTest/sxqTest-1.0-SNAPSHOT.jar > /root/sxqtest/sxqTest/nohup.out &
 
